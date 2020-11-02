3a862c95ca88ef32a4644ad41f2265caf1f22caf
Bala837 <subrahmanyamb837@gmail.com>
2020-11-02 16:19:05 +0530

    C#1

---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy
+{
+    class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dateofbirth { get; set; }
+
+        internal Student(int id, string name, DateTime dateofbirth)
+        {
+            this.id = id;
+            this.name = name;
+            this.dateofbirth = dateofbirth;
+        }
+
+    }
+    class course
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int DurationInDays { get; set; }
+        public int fees { get; set; }
+    }
+}
